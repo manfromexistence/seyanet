ff9a26d126d7af203e855b5e183b483931b0f769
Harry <ajju40959@gmail.com>
2024-04-07 11:39:43 +0000

    feat: hi

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,5 +1,2 @@
-from deep_translator import GoogleTranslator
-to_translate = 'I want to translate this text'
-translated = GoogleTranslator(source='auto', target='de').translate(to_translate)
-print(translated)
-# outpout -> Ich möchte diesen Text übersetzen
+from deep_translator import MyMemoryTranslator
+translated = MyMemoryTranslator(source="en", target="zh").translate(text='cute')
