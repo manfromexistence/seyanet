d25866ddb60de3d4b2f101c8612225e09ebd0eb3
Harry <ajju40959@gmail.com>
2024-04-07 11:11:24 +0000

    feat: python is the best

---
+++ backend/python/main.py
@@ -0,0 +1,5 @@
+from deep_translator import GoogleTranslator
+to_translate = 'I want to translate this text'
+translated = GoogleTranslator(source='auto', target='de').translate(to_translate)
+print(translated)
+# outpout -> Ich möchte diesen Text übersetzen
