4e7f43df97589b711b0a6992f8c749618621c9b9
Harry <ajju40959@gmail.com>
2024-04-07 11:40:30 +0000

    feat: hello

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,2 +1,6 @@
-from deep_translator import MyMemoryTranslator
-translated = MyMemoryTranslator(source="en", target="zh").translate(text='cute')
+from deep_translator import GoogleTranslator
+
+to_translate = 'I want to translate this text'
+translated = GoogleTranslator(source='auto', target='de').translate(to_translate)
+
+print(translated)
