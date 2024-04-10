8ac56038f63d7942a2676b95e65706a3b246e699
Man From Existence <89142466+manfromexistence@users.noreply.github.com>
2024-04-10 07:34:58 +0000

    feat: hello

--- frontend/www/apps/www/app/(app)/page.tsx
+++ frontend/www/apps/www/app/(app)/page.tsx
@@ -15,6 +15,19 @@ import {
 import { buttonVariants } from "@/registry/new-york/ui/button"
 import MailPage from "@/app/(app)/examples/mail/page"
 import translate from "@iamtraction/google-translate";
+import { CommandMenu } from "@/components/command-menu"
+import { Input } from "@/registry/default/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/registry/default/ui/select"
+import { Button } from "@/registry/default/ui/button"
+import { Plus } from "lucide-react"
 
 translate('Tu es incroyable!', { to: 'en' }).then((res: { text: any }) => {
   console.log(res.text);
@@ -26,28 +39,55 @@ export default function IndexPage() {
   return (
     <div className="container relative">
       <PageHeader>
-        <Announcement />
-        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderHeading>Project for Mostafa.</PageHeaderHeading>
         <PageHeaderDescription>
-          Beautifully designed components that you can copy and paste into your
-          apps. Accessible. Customizable. Open Source.
+          {/* Translation Management for Seyaha Website. */}
+          I am offering a comprehensive solution for your e-commerce business with a Translation Management System (TMS).
+          This system will be capable of translating various aspects of your product listings into over 25 languages, thereby expanding your global reach and accessibility.
         </PageHeaderDescription>
         <PageActions>
-          <Link href="/docs" className={cn(buttonVariants())}>
-            Get Started
+          <Link href="https://www.upwork.com/freelancers/~01b52af7a84ded5239" className={cn(buttonVariants())}>
+            Upwork
           </Link>
           <Link
             target="_blank"
             rel="noreferrer"
-            href={siteConfig.links.github}
+            href="https://github.com/manfromexistence"
             className={cn(buttonVariants({ variant: "outline" }))}
           >
             <Icons.gitHub className="mr-2 h-4 w-4" />
-            GitHub
+            Github
           </Link>
         </PageActions>
       </PageHeader>
-      <ExamplesNav className="[&>a:first-child]:text-primary" />
+
+      <div className="w-full h-[100vh] rounded-md flex items-start justify-start px-5 space-x-2">
+        <Input className="w-full" placeholder="Search your products..." />
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select by activity" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Acitivites</SelectLabel>
+              <SelectItem value="apple">Title</SelectItem>
+              <SelectItem value="banana">Description</SelectItem>
+              <SelectItem value="blueberry">Path</SelectItem>
+              <SelectItem value="grapes">Price</SelectItem>
+              <SelectItem value="pineapple">Requirement</SelectItem>
+              <SelectItem value="pineapple">Variation</SelectItem>
+              <SelectItem value="pineapple">Explusive</SelectItem>
+              <SelectItem value="pineapple">Guidence</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button variant="secondary">
+          <Plus className="h-5 w-5" />
+        </Button>
+
+      </div>
+
+      {/* <ExamplesNav className="[&>a:first-child]:text-primary" />
       <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
         <Image
           src="/examples/mail-dark.png"
@@ -68,7 +108,7 @@ export default function IndexPage() {
         <div className="overflow-hidden rounded-lg border bg-background shadow-lg">
           <MailPage />
         </div>
-      </section>
+      </section> */}
     </div>
   )
 }
