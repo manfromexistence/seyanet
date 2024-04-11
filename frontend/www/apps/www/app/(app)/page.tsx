3237d1204dd56886c8aac4662124d5e2de02431d
Man From Existence <89142466+manfromexistence@users.noreply.github.com>
2024-04-11 13:39:15 +0000

    feat: hi

--- frontend/www/apps/www/app/(app)/page.tsx
+++ frontend/www/apps/www/app/(app)/page.tsx
@@ -97,32 +97,9 @@ export default function IndexPage() {
             <Plus className="h-5 w-5" />
           </Button>
         </div>
-        <SiteNFooter />
+        {/* <SiteNFooter /> */}
 
       </div>
-
-      {/* <ExamplesNav className="[&>a:first-child]:text-primary" />
-      <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
-        <Image
-          src="/examples/mail-dark.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="hidden dark:block"
-        />
-        <Image
-          src="/examples/mail-light.png"
-          width={1280}
-          height={727}
-          alt="Mail"
-          className="block dark:hidden"
-        />
-      </section>
-      <section className="hidden md:block">
-        <div className="overflow-hidden rounded-lg border bg-background shadow-lg">
-          <MailPage />
-        </div>
-      </section> */}
     </div>
   )
 }
---
+++ frontend/www/apps/www/readme.md
@@ -0,0 +1,23 @@
+
+      {/* <ExamplesNav className="[&>a:first-child]:text-primary" />
+      <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
+        <Image
+          src="/examples/mail-dark.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="hidden dark:block"
+        />
+        <Image
+          src="/examples/mail-light.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="block dark:hidden"
+        />
+      </section>
+      <section className="hidden md:block">
+        <div className="overflow-hidden rounded-lg border bg-background shadow-lg">
+          <MailPage />
+        </div>
+      </section> */}
