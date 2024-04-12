e7f15cee398c2f95f0a2994ebb8c78a4f996d603
Man From Existence <89142466+manfromexistence@users.noreply.github.com>
2024-04-12 17:44:07 +0000

    feat: will make an backend api soon.

--- frontend/www/apps/www/components/main-nav.tsx
+++ frontend/www/apps/www/components/main-nav.tsx
@@ -22,10 +22,10 @@ export function MainNav() {
       </Link>
       <nav className="flex items-center gap-4 text-sm lg:gap-6">
         <Link
-          href="/docs"
+          href="/payme"
           className={cn(
             "transition-colors hover:text-foreground/80",
-            pathname === "/docs" ? "text-foreground" : "text-foreground/60"
+            pathname === "/payme" ? "text-foreground" : "text-foreground/60"
           )}
         >
           Hello, please pay me only 100$ through payooner.com
